416d87ff41778522bea361596c4dc78014baa6f5
SammyWebdev <sammy.webdeveloper@gmail.com>
2021-10-19 17:56:10 +0200

    faulty build

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
+// still faulty
+
 import './App.css';
 
+import * as React from "react";
+import axios from "axios";
+
+const {useState} = React;
+
+// https://randomuser.me/api/
+
+const fetchRandomData = () => {
+  return axios.get('https://randomuser.me/api')
+    .then((response) => {
+      // handle success
+      console.log(response);
+      return response
+    })
+    .catch((err) => {
+      // handle error
+      console.log(err);
+    })
+}
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <div class="navbar">
-          <ul>
-            <li>1</li>
-            <li>2</li>
-          </ul>
-        </div>
-      </header>
-      <footer className="App-footer"></footer>
+      <h1>From Coding Interview with Ben Awad:</h1>
+
+      <p>{counter}</p>
+      <button onClick={() => {
+        setCounter(counter +1);
+      }}>Increase Counter</button>
+      <button onClick={() => {
+        // none
+      }}>Fetch Random Data</button>
+
     </div>
   );
 }
