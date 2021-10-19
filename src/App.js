3b76b833d29ae739e57365b3188c3832867f18ca
SammyWebdev <sammy.webdeveloper@gmail.com>
2021-10-19 17:30:47 +0200

    TestCommit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class="navbar">
+          <ul>
+            <li>1</li>
+            <li>2</li>
+          </ul>
+        </div>
+      </header>
+      <footer className="App-footer"></footer>
+    </div>
+  );
+}
+
+export default App;
